96e4fe57121c4aa50b6a13893f0811d195da078a
6godpro <erickson5115@gmail.com>
2023-03-08 10:11:57 -0800

    addedd condition for NULL head

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,6 +12,9 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *curr = *head, *node;
 
+	if (curr == NULL)
+		return (NULL);
+
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
