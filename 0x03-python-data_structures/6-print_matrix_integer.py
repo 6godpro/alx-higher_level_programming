c097e20f968746350436c247cd50482d570b90f2
6godpro <erickson5115@gmail.com>
2023-03-11 05:44:40 -0800

    fixed print format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
     for ele in matrix:
         for i in range(len(ele)):
-            print("{:d}".format(ele[i]), end=' ')
+            print("{:d}".format(ele[i]), end='')
             if i != len(ele) - 1:
                 print(' ', end='')
         print()
