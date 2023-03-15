9e1aca51adad508100ef0d3c39c8a9beb0f948c6
Eric Eromosele <109639426+6godpro@users.noreply.github.com>
2023-03-15 12:05:05 -0700

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
 
     print("{:d} + {:d} = {}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {}\n{:d} / {:d} = {}".format(a, b, mul(a, b), a, b, div(a, b))
+    print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {}".format(a, b, div(a, b)))
