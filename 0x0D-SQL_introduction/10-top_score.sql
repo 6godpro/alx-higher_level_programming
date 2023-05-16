059ad2108475e6e0bd1c296e01fc3c94620a714d
6godpro <erickson5115@gmail.com>
2023-05-16 18:41:35 +0100

    Initial upload for tasks 0-13

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table.
+  -- Results should display both the score and the name.
+  -- Records should be ordered by score.
+SELECT score, name FROM second_table ORDER BY score DESC;
