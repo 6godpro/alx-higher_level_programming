29c26ea5c345fcf24d03d9480b21aabc391a50af
6godpro <erickson5115@gmail.com>
2023-05-17 18:17:57 +0100

    Solution for task 4. ID can't be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Creates a table id_not_null on your MySQL server with attributes id and name.
+CREATE TABLE
+IF NOT EXISTS id_not_null (
+       	      `id` INT DEFAULT 1,
+	      `name` VARCHAR(256)
+);
