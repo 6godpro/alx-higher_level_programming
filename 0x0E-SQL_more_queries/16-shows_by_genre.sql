5419bce607b99e1e0413c2efedf73d640ddf3d56
6godpro <erickson5115@gmail.com>
2023-05-17 16:18:11 -0700

    initial commit task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- Lists all shows, and all genres linked to that show,
+-- from the database hbtn_0d_tvshows.
+   -- * If a show doesn’t have a genre, display NULL in the genre column
+   -- * Each record should display: tv_shows.title - tv_genres.name
+   -- * Must be sorted in ascending order by the show title and genre name
+SELECT s.title, tg.name
+  FROM tv_shows AS s
+    INNER JOIN tv_show_genres AS tsg
+    ON s.id = tsg.show_id
+    INNER JOIN tv_genres AS tg
+    ON tsg.genre_id = tg.id
+  WHERE tg.name = 'Comedy'
+  ORDER NY s.title, tg.name;
