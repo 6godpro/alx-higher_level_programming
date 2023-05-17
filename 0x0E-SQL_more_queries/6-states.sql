7ecfb792ec62c9bf09bf812cc2bec6d693878acc
6godpro <erickson5115@gmail.com>
2023-05-17 18:53:25 +0100

    Solution for task PRIMARY KEY(`id`)

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE
 IF NOT EXISTS hbtn_0d_usa.states (
-     	 	`id` INT NOT NULL AUTO INCREMENT,
-		`name` VARCHAR(256) NOT NULL,
-		PRIMARY KEY(`id`)
+	      PRIMARY KEY(`id`),
+       	      `id` INT NOT NULL AUTO INCREMENT,
+	      `name` VARCHAR(256) NOT NULL
 );
