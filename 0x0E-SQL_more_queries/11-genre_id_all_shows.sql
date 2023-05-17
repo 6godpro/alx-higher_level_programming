6ee81a4c337f8704122fb4035f42f6035d20b01c
6godpro <erickson5115@gmail.com>
2023-05-17 21:26:54 +0100

    Solution for task 11. Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Records should display: tv_shows.title - tv_show_genres.genre_id
+-- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
