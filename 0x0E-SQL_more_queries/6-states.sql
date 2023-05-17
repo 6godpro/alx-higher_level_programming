15b133bafea45b00aff59f32cba4878155ae9f9b
6godpro <erickson5115@gmail.com>
2023-05-17 18:48:23 +0100

    Solution for task 6. States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,8 @@
 -- Creates the database hbtn_0d_usa and the table states
    -- with attributes id and name.
-  CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-  USE hbtn_0d_usa;
-  CREATE TABLE
-  IF NOT EXISTS states (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE
+IF NOT EXISTS hbtn_0d_usa.states (
      	 	`id` INT NOT NULL AUTO INCREMENT,
 		`name` VARCHAR(256) NOT NULL,
 		PRIMARY KEY(`id`)
