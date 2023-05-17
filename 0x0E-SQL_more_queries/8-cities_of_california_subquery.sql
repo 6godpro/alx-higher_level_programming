1115c6384d667007adb4ca3cccd2b5eabdfaa121
6godpro <erickson5115@gmail.com>
2023-05-17 20:14:11 +0100

    Solution for task 8. Cities of Cali

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
 USE hbtn_0d_usa;
 SELECT `id`, `name` FROM cities
-WHERE state_id IN (
+WHERE `state_id` IN (
       SELECT `id` FROM hbtn_0d_usa.states
-      WHERE `name` = 'California'
+      WHERE `name` = 'California';
       )
-ORDER BY id;
+ORDER BY `id`;
