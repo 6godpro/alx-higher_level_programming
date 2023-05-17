c25a9e17927a67f020d6e2d270ec5d5916c94a9c
6godpro <erickson5115@gmail.com>
2023-05-17 19:29:14 +0100

    Solution for task 7. cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,10 +7,10 @@
    -- * name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-       	     	PRIMARY KEY(`id`),
 		`id` INT NOT NULL AUTO_INCREMENT,
-		FOREIGN KEY(`state_id`)
-		REFERENCES hbtn_0d_usa.states(`id`),
+		PRIMARY KEY(`id`),
 		`state_id` INT NOT NULL,
 		`name` VARCHAR(256) NOT NULL
+		FOREIGN KEY(`state_id`)
+		REFERENCES hbtn_0d_usa.states(`id`),
 );
