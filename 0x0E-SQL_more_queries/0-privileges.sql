b83ce072a922bc7bb859e46c41f83b1d38432c59
6godpro <erickson5115@gmail.com>
2023-05-17 17:03:16 +0100

    fixed hostname error in task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the user_0d_1 and user_0d_2 on your server.
-SHOW GRANTS FOR 'user_0d_1'@'localost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
