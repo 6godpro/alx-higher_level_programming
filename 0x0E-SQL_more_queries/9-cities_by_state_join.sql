67dd08180afbb1ba5bce915ef32f302b86326b3b
6godpro <erickson5115@gmail.com>
2023-05-17 20:28:00 +0100

    Solution for task 9. Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Cities contained in the database hbtn_0d_usa.
-SELECT id, cities.name, states.name FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id;
