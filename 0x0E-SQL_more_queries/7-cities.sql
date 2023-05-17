f824f7f3360f2b6d6e9ea6b303ee2145b75dca0e
6godpro <erickson5115@gmail.com>
2023-05-17 19:34:12 +0100

    Solution for task 7 cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,9 +6,10 @@
       -- references to id of the states table
    -- * name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-		`id` INT NOT NULL AUTO_INCREMENT,
 		PRIMARY KEY(`id`),
+		`id` INT NOT NULL AUTO_INCREMENT,
 		`state_id` INT NOT NULL,
 		`name` VARCHAR(256) NOT NULL,
 		FOREIGN KEY(`state_id`)
