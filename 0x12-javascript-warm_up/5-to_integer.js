de16f4edf71955135dafe6bf6eb009569f4687dc
6godpro <erickson5115@gmail.com>
2023-06-13 16:40:39 +0100

    (fix)-> fixed styling

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,6 @@ const arg = process.argv.slice(2, 3).join();
 
 if (Number.isNaN(Number(arg))) {
   console.log('Not a number');
-}
-else {
+} else {
   console.log('My number:', Number(parseInt(arg)));
 }
