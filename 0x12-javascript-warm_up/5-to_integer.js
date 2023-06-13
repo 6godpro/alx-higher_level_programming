dce80844fa2b3081d32f038564aec1cc9851a4dc
6godpro <erickson5115@gmail.com>
2023-06-13 16:39:21 +0100

    Solution for task 5. An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Displays the first argument if it can be converted to an integer.
+
+const arg = process.argv.slice(2, 3).join();
+
+if (Number.isNaN(Number(arg))) {
+  console.log('Not a number');
+}
+else {
+  console.log('My number:', Number(parseInt(arg)));
+}
