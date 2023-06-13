518bdf56f766c875d45338d4be2bde5de7638681
6godpro <erickson5115@gmail.com>
2023-06-13 16:46:26 +0100

    Solution for task 6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Displays three(3) lines using a loop.
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i <= 2; i++) {
+  console.log(array[i]);
+}
