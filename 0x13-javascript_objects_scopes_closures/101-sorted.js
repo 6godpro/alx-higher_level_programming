a2ce565ec14c691030a4d46a58d6691da96bd3d6
6godpro <erickson5115@gmail.com>
2023-06-14 21:59:28 +0100

    Solution for task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@ const dict = require('./101-data.js').dict;
 
 const newDict = {};
 for (const key in dict) {
-  if (newDict[dict[key]]) {
+  if (newDict[dict[key]] != undefined) {
     newDict[dict[key]].push(key);
   } else {
     newDict[dict[key]] = [key];
