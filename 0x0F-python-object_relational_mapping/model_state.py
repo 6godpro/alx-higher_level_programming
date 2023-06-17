94a607ed23057b7ce22950b564e56a5bfcd9ad16
6godpro <erickson5115@gmail.com>
2023-06-18 00:03:06 +0100

    (new)->initial commit task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""This module contains the class definition of a State."""
+from sqlalchemy import Sequence, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state object."""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
+    name = Column(String(128), nullable=False)
