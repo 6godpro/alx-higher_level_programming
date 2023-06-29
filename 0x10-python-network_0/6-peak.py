2975ac0485d642cd6f000ae4e5d717c338301263
6godpro <erickson5115@gmail.com>
2023-06-29 22:23:22 +0100

    Solution for task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,13 +3,14 @@
     Finds a peak in a list of unsorted integers.
 """
 
+
 def find_peak(arr):
     size = len(arr)
     if size == 0:
         return None
     for i in range(len(arr)):
-        if (i >= 0 and arr[i] > arr[i - 1] and\
-            i + 1 < size and arr[i] > arr[i + 1]):
+        if (i >= 0 and arr[i] > arr[i - 1] and
+                i + 1 < size and arr[i] > arr[i + 1]):
             break
 
     return arr[i]
