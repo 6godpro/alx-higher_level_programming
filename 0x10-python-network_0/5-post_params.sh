3d8e6a4aa2626536943ed3c4900edb8cecd2790a
6godpro <erickson5115@gmail.com>
2023-06-29 23:46:25 +0100

    Solution for task

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of a POST request's response.
-curl -X POST "$1" -H "Content-Type: application/x-www-form-urlencoded" -sd "email=test@gmail.com&subject=I will always be here for PLD"
+curl -X POST "$1" -s -d "email=test@gmail.com&subject=I will always be here for PLD"
