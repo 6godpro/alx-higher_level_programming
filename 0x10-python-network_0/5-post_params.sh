3642bb264cf5908c88687b2e21bf076ff1617e31
6godpro <erickson5115@gmail.com>
2023-06-29 22:15:50 +0100

    initial commit task 6

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of a POST request's response.
-curl --request POST "$1" -sd "email=test@gmail.com&subject=I will always be here for PLD"
+curl --request POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Finds a peak in a list of unsorted integers.
+"""
+
+def find_peak(arr):
+    size = len(arr)
+    if size == 0:
+        return None
+    for i in range(len(arr)):
+        if (i >= 0 and arr[i] > arr[i - 1] and\
+            i + 1 < size and arr[i] > arr[i + 1]):
+            break
+
+    return arr[i]
