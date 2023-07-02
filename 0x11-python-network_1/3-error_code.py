120c7552be2693917e3fc04f04856572a869f676
6godpro <erickson5115@gmail.com>
2023-07-02 15:13:55 +0100

    Solution for task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Sends a request to the URL and displays the body of the response.
+
+    USAGE: ./3-error_code <URL>
+"""
+import sys
+import urllib
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.status}")
