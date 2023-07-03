edc4cdb35dff2d2b4731e3b9411533de1674216c
6godpro <erickson5115@gmail.com>
2023-07-03 07:33:19 +0100

    --

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a server and causes the server to respond with a message 'You got me!'.
-curl -sL -X PUT -H "Origin: School" -d "user_if=98" http://0.0.0.0:5000/catch_me_2
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" http://0.0.0.0:5000/catch_me_2
