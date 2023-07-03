60f1443400a3f790facef2a777c139845e3bcf91
6godpro <erickson5115@gmail.com>
2023-07-03 22:04:58 +0100

    (fix)->reordered variable unpacking

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = "https://api.github.com/search/commits"
     try:
-        user, repo = sys.argv[1:]
+        repo, user = sys.argv[1:]
         url += f"?q=repo:{user}/{repo}&per_page=10&sort=committer-date"
         response = requests.get(url)
         for item in (response.json().get('items')):
