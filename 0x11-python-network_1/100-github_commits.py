40bc94bf296fa9e52ab2d9b8eb746011bfa3d9a2
6godpro <erickson5115@gmail.com>
2023-07-03 09:42:27 +0100

    initial commit task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-
-
+"""
+    Retrieve 10 commits from a repository sorted from recent to oldest.
+"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}"
-    response = requests.get('')
+    url = "https://api.github.com/search/commits"
+    user, repo = sys.argv[1:]
+    url += f"?q=repo:{user}/{repo}&per_page=10&sort=committer-date"
+    response = requests.get(url)
+    try:
+        for item in (response.json().get('items')):
+            print("{}: {}".format(
+                item['sha'],
+                item['commit']['author']['name'])
+                  )
+    except KeyError:
+        pass
