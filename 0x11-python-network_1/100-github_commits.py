4163099407ec21e7cbea4d93361f064c89b2831e
6godpro <erickson5115@gmail.com>
2023-07-03 22:02:11 +0100

    (fix)->moved variable unpacking into try block

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,14 +8,14 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/search/commits"
-    user, repo = sys.argv[1:]
-    url += f"?q=repo:{user}/{repo}&per_page=10&sort=committer-date"
-    response = requests.get(url)
     try:
+        user, repo = sys.argv[1:]
+        url += f"?q=repo:{user}/{repo}&per_page=10&sort=committer-date"
+        response = requests.get(url)
         for item in (response.json().get('items')):
             print("{}: {}".format(
                 item['sha'],
                 item['commit']['author']['name'])
                   )
-    except KeyError:
+    except Exception as e:
         pass
