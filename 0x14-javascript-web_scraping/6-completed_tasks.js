2e8e247d2d52c112beffa503166cda1d6dbf033e
6godpro <erickson5115@gmail.com>
2023-07-25 22:25:02 +0100

    minor refactoring

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,17 @@
 const request = require('request');
 const completed = {};
 request.get(process.argv[2], (err, res, body) => {
-  err && console.log(err);
-  const todos = JSON.parse(body);
-  todos.forEach((task) => {
-    if (task.completed && completed[task.userId] === undefined) {
-      completed[task.userId] = 1;
-    } else {
-      task.completed && completed[task.userId]++;
-    }
-  });
-  console.log(completed);
+  if (!err) {
+    const todos = JSON.parse(body);
+    todos.forEach((task) => {
+      if (task.completed) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId] += 1;
+        }
+      }
+    });
+    console.log(completed);
+  }
 });
