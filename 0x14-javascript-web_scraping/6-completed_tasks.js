d543169b804300d05350b21557486259305d3221
6godpro <erickson5115@gmail.com>
2023-07-25 18:20:04 +0100

    initial upload task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const completed = {};
+request.get(`${process.argv[2]}`, (err, res, body) => {
+  err && console.log(err);
+  const todos = JSON.parse(body);
+  todos.forEach((task) => {
+    if (task.completed && completed[task.userId] === undefined) {
+      completed[task.userId] = 1;
+    } else {
+      task.completed && completed[task.userId]++;
+    }
+  });
+  console.log(completed);
+});
