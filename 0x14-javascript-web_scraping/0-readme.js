d3199688a6d8f932416eabd5f440132e31a37dc9
6godpro <erickson5115@gmail.com>
2023-07-25 13:30:52 +0100

    (fix)-> reformatted error output

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const fileName = process.argv[2];
 fs.readFile(fileName, { encoding: 'utf-8' }, (err, data) => {
   if (err) {
-    console.log(err);
+    console.log(`{ Error: ${err.message} 
+  errno: ${err.errno},
+  code: '${err.code}',
+  syscall: '${err.syscall}',
+  path: '${err.path}' }`);
   } else {
     console.log(data);
   }
